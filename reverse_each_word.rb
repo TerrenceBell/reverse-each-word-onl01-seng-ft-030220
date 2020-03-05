d35792f3eb82c16fd2d52f8978b812922c75ea93
Terrence Bell <terrencekbell@gmail.com>
2020-03-05 05:41:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(string)
   empty_array = string.split(" ")
 array= []
-empty_array.each do |string|
+empty_array.collect do |string|
   array.push(string.reverse)
 end
  array.join(" ")
  end
+ 
+ 
